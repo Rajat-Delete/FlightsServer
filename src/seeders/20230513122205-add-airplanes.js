184707aef99a12dedf1322f0b4725f8b276bf37b
Rajat Sharma <rajatxd007@gmail.com>
2023-05-13 18:46:02 +0530

    Added seeders for airplanes

---
+++ src/seeders/20230513122205-add-airplanes.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Op } = require('sequelize')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('airplanes',[
+      {
+        'modelNumber': 'airbus380',
+        'capacity': '200',
+        createdAt : new Date(),
+        updatedAt: new Date()
+      },
+      {
+        'modelNumber': 'airbus320neo',
+        'capacity': '200',
+        createdAt : new Date(),
+        updatedAt: new Date()
+      }
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    //If we keep the object plain empty then all entries will be deleted
+
+    await queryInterface.bulkDelete('airplanes', {[Op.or] : [
+      {'modelNumber':'airbus380'},
+      {'modelNumber': 'airbus320neo'}
+    ]});
+  }
+};
